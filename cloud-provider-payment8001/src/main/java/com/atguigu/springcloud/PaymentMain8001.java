3691e3c8b8c2ca6fd193abd0c370fcf0af499ec6
jbw <jb@98game.cn>
2020-05-22 15:20:50 +0800

    Cloud-provider-payment8001 微服务提供者Module模块

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JiangBin
+ * @create 2020-05-22 14:32
+ */
+
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
