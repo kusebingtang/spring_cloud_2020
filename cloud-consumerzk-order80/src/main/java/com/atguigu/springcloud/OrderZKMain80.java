1171f0158cd4c7d7cbdca595b35c4fcd50cb1734
jbw <jb@98game.cn>
2020-05-23 12:14:24 +0800

    使用zookeeper 消费端工程调用

---
+++ cloud-consumerzk-order80/src/main/java/com/atguigu/springcloud/OrderZKMain80.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author JiangBin
+ * @create 2020-05-23 12:10
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderZKMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZKMain80.class, args);
+    }
+}
