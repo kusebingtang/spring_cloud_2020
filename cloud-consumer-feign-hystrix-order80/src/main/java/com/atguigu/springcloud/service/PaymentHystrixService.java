892bc9d5b13aa0912b3838c1c9a852e987d75847
jbw <jb@98game.cn>
2020-05-25 10:34:22 +0800

    通配 全局服务降级 FeignFallback

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/service/PaymentHystrixService.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/service/PaymentHystrixService.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
  * @create 2020-05-24 17:22
  */
 @Component
-@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+//@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT" ,fallback = PaymentFallbackService.class)
 public interface PaymentHystrixService {
     @GetMapping("/payment/hystrix/ok/{id}")
     public String paymentInfo_OK(@PathVariable("id") Integer id);
