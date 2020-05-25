d658cff1238f6be71d6ce44a80cb02a28f650a89
jbw <jb@98game.cn>
2020-05-25 11:48:52 +0800

    hystrix dashboard demo

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/srpingcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,17 @@
+package com.atguigu.srpingcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author JiangBin
+ * @create 2020-05-25 11:33
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
